e545be4057e1ba00581c9660dbfa6a70c37c8cf9
erlanamanatov <erlanamanatov@gmail.com>
2018-07-04 09:02:09 +0600

    Implement DatePicker and TimePicker in one DialogFragment

--- DateandTimePicker/app/src/main/java/com/erkprog/dateandtimepicker/MainFragment.java
+++ DateandTimePicker/app/src/main/java/com/erkprog/dateandtimepicker/MainFragment.java
@@ -1,15 +1,23 @@
 package com.erkprog.dateandtimepicker;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Date;
+
 public class MainFragment extends Fragment implements View.OnClickListener{
+  private static final String DIALOG = "Date and time dialog";
+  public static final int REQUEST_CODE = 0;
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -28,11 +36,25 @@ public class MainFragment extends Fragment implements View.OnClickListener{
   public void onClick(View v) {
     switch (v.getId()){
       case R.id.dialog_button:
-        Toast.makeText(getActivity(), "CLicked", Toast.LENGTH_SHORT).show();
+        FragmentManager fm = getFragmentManager();
+        PickerFragment dialogFragment = PickerFragment.newInstance(new Date());
+        dialogFragment.setTargetFragment(MainFragment.this, REQUEST_CODE);
+        dialogFragment.show(fm, DIALOG);
         break;
       default:
         break;
     }
+  }
 
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (resultCode != Activity.RESULT_OK){
+      return;
+    }
+
+    if (requestCode == REQUEST_CODE){
+      Date date = (Date) data.getSerializableExtra(PickerFragment.EXTRA_DATE);
+      Toast.makeText(getActivity(), date.toString(), Toast.LENGTH_SHORT).show();
+    }
   }
 }
