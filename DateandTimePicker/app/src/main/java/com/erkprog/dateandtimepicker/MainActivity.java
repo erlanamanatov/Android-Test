8a611cf243f807ef076014a1ec67138dcd071896
erlanamanatov <erlanamanatov@gmail.com>
2018-07-04 06:06:16 +0600

    Date and Time Picker project created with Fragemnt

---
+++ DateandTimePicker/app/src/main/java/com/erkprog/dateandtimepicker/MainActivity.java
@@ -0,0 +1,25 @@
+package com.erkprog.dateandtimepicker;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = fm.findFragmentById(R.id.main_fragment_container);
+
+    if (fragment == null) {
+      fragment = new MainFragment();
+      fm.beginTransaction()
+          .add(R.id.main_fragment_container, fragment)
+          .commit();
+    }
+  }
+}
