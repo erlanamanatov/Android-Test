bbf9f312c46ce555e518bf06a99b7b4130b26afe
erlanamanatov <erlanamanatov@gmail.com>
2018-07-04 06:10:40 +0600

    Add button to show toast

--- DateandTimePicker/app/src/main/java/com/erkprog/dateandtimepicker/MainFragment.java
+++ DateandTimePicker/app/src/main/java/com/erkprog/dateandtimepicker/MainFragment.java
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener{
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,6 +19,20 @@ public class MainFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
     View v = inflater.inflate(R.layout.fragment_main, container, false);
+    Button dbutton = v.findViewById(R.id.dialog_button);
+    dbutton.setOnClickListener(this);
     return v;
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()){
+      case R.id.dialog_button:
+        Toast.makeText(getActivity(), "CLicked", Toast.LENGTH_SHORT).show();
+        break;
+      default:
+        break;
+    }
+
   }
 }
